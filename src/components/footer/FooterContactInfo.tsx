1dd2199163c31e7cf01c60abd3a24e6f8ac8693a
Hardiksinh <110206020+harryjadeja1111@users.noreply.github.com>
2025-05-26 14:06:41 +0100

    metatag update

--- src/components/footer/FooterContactInfo.tsx
+++ src/components/footer/FooterContactInfo.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { MapPin, Phone, Mail, Clock } from "lucide-react";
 
@@ -8,14 +7,17 @@ const FooterContactInfo = () => {
       <h3 className="font-playfair text-lg font-semibold mb-6">Contact Info</h3>
       <ul className="space-y-4">
         <li className="flex">
-          <MapPin size={18} className="text-travel-gold mr-3 flex-shrink-0 mt-1" />
+          <MapPin
+            size={18}
+            className="text-travel-gold mr-3 flex-shrink-0 mt-1"
+          />
           <span className="text-gray-300">
             22 Wheelers Lane, Bradville, Milton Keynes, England, MK137HW
           </span>
         </li>
         <li className="flex">
           <Phone size={18} className="text-travel-gold mr-3 flex-shrink-0" />
-          <span className="text-gray-300">+44 7765793462</span>
+          <span className="text-gray-300">+44 7765793462 Hardiksinh</span>
         </li>
         <li className="flex">
           <Mail size={18} className="text-travel-gold mr-3 flex-shrink-0" />
